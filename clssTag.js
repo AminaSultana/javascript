2184672080ad555594e2d60c916fd884e41bbf98
AminaSultana <lilisultana53@gmail.com>
2022-03-19 05:41:11 +0530

    message

---
+++ clssTag.js
@@ -0,0 +1,17 @@
+let m = document.getElementsByClassName('list1')
+m[0].style.fontWeight = 'bold'
+let li1 = document.getElementsByTagName('li')
+li1[4].style.backgroundColor = 'red'
+
+/* let li = document.getElementsByTagName('li')
+console.log(li)
+li[2].style.backgroundColor = 'yellow'
+
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight = 'bold'
+    li[i].style.backgroundColor = 'blue'
+    li[i].textContent = i
+}
+
+
+ */
