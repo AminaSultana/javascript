6b0f8352d747cff04ddb00de6b4456984fd6ea95
AminaSultana <lilisultana53@gmail.com>
2022-03-19 08:48:35 +0530

    task 6

--- clssTag.js
+++ clssTag.js
@@ -1,9 +1,9 @@
-let m = document.getElementsByClassName('list1')
+/* let m = document.getElementsByClassName('list1')
 m[0].style.fontWeight = 'bold'
 let li1 = document.getElementsByTagName('li')
-li1[4].style.backgroundColor = 'red'
+li1[4].style.backgroundColor = 'red' */
 
-/* let li = document.getElementsByTagName('li')
+ /* let li = document.getElementsByTagName('li')
 console.log(li)
 li[2].style.backgroundColor = 'yellow'
 
@@ -11,7 +11,21 @@ for(let i=0;i<li.length;i++){
     li[i].style.fontWeight = 'bold'
     li[i].style.backgroundColor = 'blue'
     li[i].textContent = i
+} */
+
+/* 
+let m = document.querySelector('.list-group:nth-child(2)');
+m.style.backgrounColor = 'green'
+
+let f = document.querySelector('.list-group:nth-child(3)')
+f.textContent = ""
+  */
+
+
+let sec = document.querySelectorAll('.list-group');
+sec[2].style.color = 'green';
+
+let odd = document.querySelectorAll('.list-group:nth-child(odd)');
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green'
 }
-
-
- */
